ca7e6d9a21203e5fb715058e7c23bf200efbbe81
MostafaAmr45 <43935726+MostafaAmr45@users.noreply.github.com>
2021-06-03 14:43:23 +0200

    add is_bit_set() & is_bit_clr() macros

--- macros.h
+++ macros.h
@@ -6,12 +6,15 @@
  */
 
 #ifndef MACROS_H_
+#define MACROS_H_
+
 #define set_bit(Data,Bit) Data|= (1<<Bit)
 #define clr_bit(Data,Bit) Data&= ~(1<<Bit)
 #define toggle_bit(Data,Bit) Data^= (1<<Bit)
 #define get_bit(Data,Bit) (Data>>Bit)&1
 #define assign_bit(Data,Bit,Val) (Val==1)? (set_bit(Data,Bit)):(clr_bit(Data,Bit))
-#define MACROS_H_
+#define is_bit_set(Data,Bit)  (Data&(1<<Bit))>>Bit
+#define is_bit_clr(Data,Bit)  !((Data&(1<<Bit))>>Bit)
 
 
 
