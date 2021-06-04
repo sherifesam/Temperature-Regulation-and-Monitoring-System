9a09728099d25079609b8d465b751979fffb6c06
mohammedelsayed412 <mohammedelsayed41298@ gmail.com>
2021-06-04 05:00:02 +0200

    Heater_CNRL

--- SERVICE/Heat_CTRL.h
+++ SERVICE/Heat_CTRL.h
@@ -2,5 +2,6 @@
 #define Heat_CNRL_H_
 
 void Heat_CNRL_Duty_Cycle(s8 Current_Temp, s8 Set_Temp, f32 * Duty_Value);
- 
+void Heat_CNRL_Duty_Value_Map(f32 Duty_Value , u16 * PWM_Duty );
+
 #endif /* Heat_CNRL_H_ */
