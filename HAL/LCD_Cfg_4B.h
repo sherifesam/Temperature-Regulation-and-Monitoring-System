8bb588d309d70ab66850a3604a67f5cd97293db6
Ahmed Khaled <ahmed.khaled0811@gmail.com>
2021-06-05 19:48:32 +0200

    add initial version of lcd rtos 4bit
    sorry

---
+++ HAL/LCD_Cfg_4B.h
@@ -0,0 +1,21 @@
+/*
+ * LCD_Extra.h
+ *
+ *  Created on: Aug 16, 2018
+ *      Author: hp
+ */
+
+#ifndef LCD_EXTRA_H_
+#define LCD_EXTRA_H_
+
+
+#define RS_PORT			PORTC
+#define RS_PIN			PIN0
+#define RW_PORT			PORTC
+#define RW_PIN			PIN1
+#define ENABLE_PORT		PORTC
+#define ENABLE_PIN		PIN2
+#define DATA_PORT		PORTD
+
+
+#endif /* LCD_EXTRA_H_ */
