24e03a44bd4d3b1729bd882417dee6990639fbdb
MostafaAmr45 <43935726+MostafaAmr45@users.noreply.github.com>
2021-06-07 00:44:25 +0200

    SPI MISRA V1

--- MCAL/SPI.h
+++ MCAL/SPI.h
@@ -8,8 +8,8 @@
 #ifndef SPI_H_
 #define SPI_H_
 
-void SPI_SendData(s8 u8data);
-s8 SPI_ReceiveData();
+void SPI_SendData(u8 u8data);
+u8 SPI_ReceiveData(void);
 void SPI_init(void);
 
 #endif /* SPI_H_ */
