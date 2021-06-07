c591254e7bc8a04a432864cbb471c9ee09e96961
MostafaAmr45 <43935726+MostafaAmr45@users.noreply.github.com>
2021-06-07 06:21:44 +0200

    HEAT_CTRL MISRA V1

--- SERVICE/Heat_CTRL.c
+++ SERVICE/Heat_CTRL.c
@@ -4,28 +4,39 @@
 #include "../macros.h"
 
 #include "../HAL/TC72_Driver.h"
-#include "../MCAL/Timer.h"
-#include "../MCAL/ADC.h"
-#include "../MCAL/Timer.h"
-
-#include <util/delay.h>
-
+#include "../MCAL/PWM_TIMER0/TIMER0_config.h"
+#include "../MCAL/PWM_TIMER0/TIMER0_interface.h"
+#include "../MCAL/ADC_OS.h"
+#include "Heat_CTRL.h"
 
 
-void Heat_CNRL_Duty_Cycle(s8 Current_Temp, s8 Set_Temp, f32* Duty_Value)
+
+void Heat_CNRL_Duty_Cycle(u8 Current_Temp, u8 Set_Temp)
 {
 	f32 Temp_Volt = 0;
 	f32 Potontiometer_Volt = 0;
-
-	Temp_Volt = (( Set_Temp - Current_Temp) / 100.0 ) * 10.0 ;
-	Potontiometer_Volt  = ADC_GetValue() ;
-	*Duty_Value = ((( Potontiometer_Volt * 2.0 ) / 10.0 ) * Temp_Volt ) / 10.0;
+	f32 Duty_value=0;
+	u8 PWM_dutyCycle=0;
+	
+	Temp_Volt = (( (f32)Set_Temp - (f32)Current_Temp) / (f32)100.0 ) * (f32)10.0 ;
+	Potontiometer_Volt  = ADC_OS_GetValue();
+	Duty_value = ((( Potontiometer_Volt * (f32)2.0 ) / (f32)10.0 ) * Temp_Volt ) / (f32)10.0;
+	PWM_dutyCycle= Heat_CNRL_Duty_Value_Map(Duty_value);
+	TIMER0_void_fastPWM(PWM_dutyCycle);
+	
 }
 
 
-void Heat_CNRL_Duty_Value_Map(f32 Duty_Value , u16 * PWM_Duty )
+u8 Heat_CNRL_Duty_Value_Map(f32 Duty_Value)
 {
-	*PWM_Duty = ( 255 - (u16)(Duty_Value * 255));
+	u8 PWM_duty = ((u8)((u8)Duty_Value * (u8)255));
+	return PWM_duty;
+	
+}
+
+void stop_pwm(void)
+{
+	TIMER0_void_fastPWM(STOP_PWM_VALUE);
 }
 
 
--- SERVICE/Heat_CTRL.h
+++ SERVICE/Heat_CTRL.h
@@ -1,7 +1,11 @@
 #ifndef Heat_CNRL_H_
 #define Heat_CNRL_H_
 
-void Heat_CNRL_Duty_Cycle(s8 Current_Temp, s8 Set_Temp, f32 * Duty_Value);
-void Heat_CNRL_Duty_Value_Map(f32 Duty_Value , u16 * PWM_Duty );
+
+#define STOP_PWM_VALUE	((u8)255)
+
+void Heat_CNRL_Duty_Cycle(u8 Current_Temp, u8 Set_Temp);
+u8 Heat_CNRL_Duty_Value_Map(f32 Duty_Value);
+void stop_pwm(void);
 
 #endif /* Heat_CNRL_H_ */
