4b2c2bd9d51a029dac90a8a60fb813c1c8a9bf1e
Ahmed Khaled <ahmed.khaled0811@gmail.com>
2021-06-07 16:27:12 +0200

    DSP MGR MISRA V1

--- SERVICE/DSP_MGR.c
+++ SERVICE/DSP_MGR.c
@@ -1,6 +1,7 @@
 
 #include "DSP_MGR.h"
 
+
 char normal[] =    {'N', 'O', 'R', 'M', 'A', 'L', ' ', ' ', ' ', '\0'};
 char standby[] =   {'S', 'T', 'A', 'N', 'D', 'B', 'Y', ' ', ' ', '\0'};
 char operation[] = {'O', 'P', 'E', 'R', 'A', 'T', 'I', 'O', 'N', '\0'};
@@ -14,218 +15,279 @@ char initial_state[] = "STATE:STANDBY";
 char* modes[] = {normal, standby, operation, error, initial_state, initial_temp, welcome};
 
 static u8 DISP_FIN_WRITING = 0;
-
 void DISP_WRITE_STATE(u8 mode_id, u8 row, u8 col, u8 *i)
 {
 	u8 code = getCursorCommand(row, col+(*i));
-	while(LOC_vidSendCommand(code) != 1);
+	while(LOC_vidSendCommand(code) != (u8)1)
+	{
+		/* DO NOTHING */
+	}
 	char *mode = modes[mode_id];
 	if(is_wreq_available())
 	{
-		u8 char_to_be_written = *(mode+(*i));
-		if(char_to_be_written != '\0')
+		/* [MISRA VIOLATION] RULE(17.4): It is more convenient to write it this way, and we are sure that i will not pass the possible limit*/
+		u8 char_to_be_written = *(mode + (*i));
+		if(char_to_be_written != (u8)'\0')
 		{
-			DISP_FIN_WRITING = 0;
+			DISP_FIN_WRITING = (u8)0;
 			LCD_OS_WriteChar(char_to_be_written);
-			(*i) = (*i) + 1;
+			(*i) = (*i) + (u8)1;
 		}
 		else
 		{
-			(*i) = 0;
-			DISP_FIN_WRITING = 1;
+			(*i) = (u8)0;
+			DISP_FIN_WRITING = (u8)1;
 		}
 	}
 }
 
 void DISP_WRITE_TEMP(u8 set_temp, u8 crt_temp)
 {
-	// working on set_temp...
-	u8 code;
+	/* working on set_temp */
+	u8 cursor_code = (u8)0;
 	static u8 temp_turn = 0;
-	u8 set_temp_ones = set_temp % 10;
-	u8 set_temp_tens = set_temp / 10;
-	static u8 index = 0;
-	if(is_wreq_available() && temp_turn == 0)
+	u8 set_temp_ones = (u8)(set_temp % (u8)10);
+	u8 set_temp_tens = (u8)(set_temp / (u8)10);
+	static u8 index = (u8)0;
+	u8 check_wreq = (u8)is_wreq_available();
+	if((check_wreq == (u8)1) && (temp_turn == (u8)0))
 	{
-		if(index == 0)
+		if(index == (u8)0)
 		{
-			code = getCursorCommand(1, 5);
-			while(LOC_vidSendCommand(code) != 1);
-			LCD_OS_WriteChar(set_temp_tens+48);	
+			cursor_code = getCursorCommand((u8)1, (u8)5);
+			while(LOC_vidSendCommand(cursor_code) != (u8)1)
+			{
+				/* DO NOTHING */
+			}
+			LCD_OS_WriteChar(set_temp_tens+(u8)48);
 			index++;
 		}
 		else
 		{
-			code = getCursorCommand(1, 6);
-			while(LOC_vidSendCommand(code) != 1);
-			LCD_OS_WriteChar(set_temp_ones+48);
-			index = 0;
+			cursor_code = getCursorCommand((u8)1, (u8)6);
+			while(LOC_vidSendCommand(cursor_code) != (u8)1)
+			{
+				/* DO NOTHIBG */
+			}
+			LCD_OS_WriteChar(set_temp_ones+(u8)48);
+			index = (u8)0;
 			temp_turn++;
 		}
-		
+
 	}
-	// working on crt_temp...
-	u8 crt_temp_ones = crt_temp % 10;
-	u8 crt_temp_tens = crt_temp / 10;
-	static u8 index2 = 0;
-	if(is_wreq_available() && temp_turn == 1)
+	/* working on crt_temp */
+	u8 crt_temp_ones = (u8)(crt_temp % (u8)10);
+	u8 crt_temp_tens = (u8)(crt_temp / (u8)10);
+	static u8 index2 = (u8)0;
+	check_wreq = (u8)is_wreq_available();
+	if((check_wreq == (u8)1) && (temp_turn == (u8)1))
 	{
-		if(index2 == 0)
+		if(index2 == (u8)0)
 		{
-			code = getCursorCommand(1, 15);
-			while(LOC_vidSendCommand(code) != 1);
-			LCD_OS_WriteChar(crt_temp_tens+48);
+			cursor_code = getCursorCommand((u8)1, (u8)15);
+			while(LOC_vidSendCommand(cursor_code) != (u8)1)
+			{
+				/* DO NOTHING */
+			}
+			LCD_OS_WriteChar(crt_temp_tens+(u8)48);
 			index2++;
 		}
 		else
 		{
-			code = getCursorCommand(1, 16);
-			while(LOC_vidSendCommand(code) != 1);
-			LCD_OS_WriteChar(crt_temp_ones+48);
-			index2 = 0;
-			temp_turn = 0;
+			cursor_code = getCursorCommand((u8)1, (u8)16);
+			while(LOC_vidSendCommand(cursor_code) != (u8)1)
+			{
+				/* DO NOTHING */
+			}
+			LCD_OS_WriteChar(crt_temp_ones+(u8)48);
+			index2 = (u8)0;
+			temp_turn = (u8)0;
 		}
 	}
 }
 
+
+/* [MISRA VIOLATION] RULE(16.7): It is an RTOS API and a standard definition*/
 void WELCOME_OS_TASK(void* pvoid)
 {
-	u8 i = 0;
 	u8 WELCOME_STATE = 0;
 	u8 WELCOME_IT = 0;
 	u8 WELCOME_CTR = 0;
 
 	while(1)
 	{
-		//DISP_WRITE_STATE(WELCOME_ID, 1, 1, &i);
 		switch(WELCOME_STATE)
 		{
-			case 0:
+			case (u8)0:
 			{
-				DISP_WRITE_STATE(WELCOME_ID, 1, 1, &WELCOME_IT);
-				if(IS_WRITING_FINISHED() == 1)
+				DISP_WRITE_STATE(WELCOME_ID, (u8)1, (u8)1, &WELCOME_IT);
+				if((IS_WRITING_FINISHED() == (u8)1))
 				{
-					WELCOME_STATE++;	
-				}
-			}
-			break;
-			
-			case 1:
-			{
-				while(LOC_vidSendCommand(0x18) != 1);
-				WELCOME_CTR++;
-				if(WELCOME_CTR == 9)
-				{
-					WELCOME_CTR = 0;
 					WELCOME_STATE++;
 				}
 			}
 			break;
-			case 2:
+
+
+			case (u8)1:
 			{
-				while(LOC_vidSendCommand(0x1c) != 1);
-				WELCOME_CTR++;
-				if(WELCOME_CTR == 9)
+				while(LOC_vidSendCommand((u8)0x18) != (u8)1)
 				{
-					WELCOME_CTR = 0;
+					/* DO NOTHING */
+				}
+				WELCOME_CTR++;
+				if(WELCOME_CTR == (u8)9)
+				{
+					WELCOME_CTR = (u8)0;
 					WELCOME_STATE++;
 				}
 			}
 			break;
-			case 3:
+			case (u8)2:
 			{
-				while(LOC_vidSendCommand(0x18) != 1);
-				WELCOME_CTR++;
-				if(WELCOME_CTR == 9)
+				while(LOC_vidSendCommand((u8)0x1c) != (u8)1)
 				{
-					WELCOME_CTR = 0;
-					WELCOME_STATE++;
+					/* DO NOTHING */
 				}
-			}
-			case 4:
-			{
-				while(LOC_vidSendCommand(0x1c) != 1);
 				WELCOME_CTR++;
-				if(WELCOME_CTR == 9)
+				if(WELCOME_CTR == (u8)9)
 				{
-					WELCOME_CTR = 0;
+					WELCOME_CTR = (u8)0;
 					WELCOME_STATE++;
 				}
 			}
 			break;
-			case 5:
+			case (u8)3:
 			{
-				while(LOC_vidSendCommand(0x18) != 1);
-				WELCOME_CTR++;
-				if(WELCOME_CTR == 9)
+				while(LOC_vidSendCommand((u8)0x18) != (u8)1)
 				{
-					WELCOME_CTR = 0;
+					/* DO NOTHING */
+				}
+				WELCOME_CTR++;
+				if(WELCOME_CTR == (u8)9)
+				{
+					WELCOME_CTR = (u8)0;
+					WELCOME_STATE++;
+				}
+				{
+					while(LOC_vidSendCommand((u8)0x1c) != (u8)1)
+					{
+						/* DO NOTHING */
+					}
+					WELCOME_CTR++;
+					if(WELCOME_CTR == (u8)9)
+					{
+						WELCOME_CTR = (u8)0;
+						WELCOME_STATE++;
+					}
+				}
+				break;
+			}
+
+			case (u8)4:
+			{
+				while(LOC_vidSendCommand((u8)0x1c) != (u8)1)
+				{
+
+				}
+				WELCOME_CTR++;
+				if(WELCOME_CTR == (u8)9)
+				{
+					WELCOME_CTR = (u8)0;
 					WELCOME_STATE++;
 				}
 			}
 			break;
-			case 6:
+			case (u8)5:
 			{
-				while(LOC_vidSendCommand(0x1c) != 1);
-				WELCOME_CTR++;
-				if(WELCOME_CTR == 9)
+				while(LOC_vidSendCommand((u8)0x18) != (u8)1)
 				{
-					WELCOME_CTR = 0;
+					/* DO NOTHING */
+				}
+				WELCOME_CTR++;
+				if(WELCOME_CTR == (u8)9)
+				{
+					WELCOME_CTR = (u8)0;
 					WELCOME_STATE++;
 				}
 			}
 			break;
-			case 7:
+			case(u8)6:
 			{
-				while(LOC_vidSendCommand(0x18) != 1);
-				WELCOME_CTR++;
-				if(WELCOME_CTR == 9)
+				while(LOC_vidSendCommand((u8)0x1c) != (u8)1)
 				{
-					WELCOME_CTR = 0;
+					/* DO NOTHING */
+				}
+				WELCOME_CTR++;
+				if(WELCOME_CTR == (u8)9)
+				{
+					WELCOME_CTR = (u8)0;
 					WELCOME_STATE++;
 				}
 			}
 			break;
-			case 8:
+			case (u8)7:
 			{
-				while(LOC_vidSendCommand(0x1c) != 1);
-				WELCOME_CTR++;
-				if(WELCOME_CTR == 10)
+				while(LOC_vidSendCommand((u8)0x18) != (u8)1)
 				{
-					WELCOME_CTR = 0;
+					/* DO NOTHING */
+				}
+				WELCOME_CTR++;
+				if(WELCOME_CTR == (u8)9)
+				{
+					WELCOME_CTR = (u8)0;
 					WELCOME_STATE++;
 				}
 			}
 			break;
-			case 9:
+			case (u8)8:
 			{
-				WELCOME_CTR++;
-				if(WELCOME_CTR == 10)
+				while(LOC_vidSendCommand((u8)0x1c) != (u8)1)
 				{
-					WELCOME_CTR = 0;
+					/* DO NOTHING */
+				}
+				WELCOME_CTR++;
+				if(WELCOME_CTR == (u8)10)
+				{
+					WELCOME_CTR = (u8)0;
 					WELCOME_STATE++;
 				}
 			}
 			break;
-			case 10:
+			case (u8)9:
 			{
-				while(LOC_vidSendCommand(0x01) != 1);
+				WELCOME_CTR++;
+				if(WELCOME_CTR == (u8)10)
+				{
+					WELCOME_CTR = (u8)0;
+					WELCOME_STATE++;
+				}
+			}
+			break;
+			case (u8)10:
+			{
+				while(LOC_vidSendCommand((u8)0x01) != (u8)1)
+				{
+					/* DO NOTHING */
+				}
 				WELCOME_STATE++;
 			}
 			break;
+			default:
+			break;
 		}
-		vTaskDelay(100);	
+		/* [MISRA VIOLATION] RULE(8.1 8.6): It is an RTOS API and a standard definition */
+		vTaskDelay(100);
 	}
-	
 }
 
 u8 IS_WRITING_FINISHED(void)
 {
-	u8 cond = 0;
-	if(DISP_FIN_WRITING == 1)
+	u8 cond = (u8)0;
+	if(DISP_FIN_WRITING == (u8)1)
 	{
-		cond = 1;
-		DISP_FIN_WRITING = 0;
+		cond = (u8)1;
+		DISP_FIN_WRITING = (u8)0;
 	}
 	return cond;
 }
--- SERVICE/DSP_MGR.h
+++ SERVICE/DSP_MGR.h
@@ -1,20 +1,18 @@
 #ifndef SERVICE_DSP_OS_H_
 #define SERVICE_DSP_OS_H_
 
+#include "../STD_Types.h"
 #include "../HAL/LCD_OS.h"
 #include "../HAL/Keypad_OS.h"
 
-#define NORMAL_ID           0
-#define STANDBY_ID          1
-#define OPERATION_ID        2
-#define ERROR_ID            3
-#define INITIAL_STATE_ID    4
-#define INITIAL_TEMP_ID     5
-#define WELCOME_ID     6
+#define NORMAL_ID           ((u8)0)
+#define STANDBY_ID          ((u8)1)
+#define OPERATION_ID        ((u8)2)
+#define ERROR_ID            ((u8)3)
+#define INITIAL_STATE_ID    ((u8)4)
+#define INITIAL_TEMP_ID     ((u8)5)
+#define WELCOME_ID			((u8)6)
 
-
-//char set_temp[] = {'1', '2', '\0'};
-//char crt_temp[] = {'3', '4', '\0'};
 
 void DISP_WRITE_STATE(u8 mode_id, u8 row, u8 col, u8 *i);
 void DISP_WRITE_TEMP(u8 set_temp, u8 crt_temp);
